0fa081c8edbf5069f502d2189a65a3c2075ec6ac
tadahayamiz <tadahaya@gmail.com>
2023-08-03 01:53:23 +0900

    modify init

--- faersutil/__init__.py
+++ faersutil/__init__.py
@@ -4,10 +4,9 @@ Created on Wed Dec 25 15:46:32 2019
 
 @author: tadahaya
 """
-from .faersutil import FAERS
 
-__copyright__    = 'Copyright (C) 2020 MIZUNO Tadahaya'
+__copyright__    = 'Copyright (C) 2023 Tadahaya Mizuno'
 __version__      = '0.0.1'
 __license__      = 'MIT'
-__author__       = 'MIZUNO Tadahaya'
+__author__       = 'Tadahaya Mizuno'
 __author_email__ = 'tadahaya@gmail.com'
